e1163963bf29ed788d2b10cdd56d15be99fd38c2
KarolisGaiv <kgaivenis@gmail.com>
2021-03-31 20:25:54 +0300

    Add funcionality to check multiple words

--- sub_strings/strings.rb
+++ sub_strings/strings.rb
@@ -2,12 +2,19 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(phrase, dictionary)
     result = {}
+    array = phrase.split(" ")
     dictionary.map do |word|
-        if phrase.include? word
-            result[word] = 1
+        array.map do |element|
+            if element.downcase.include? word # 3 stepas
+                if result[word] != nil
+                    result[word] += 1
+                else
+                    result[word] = 1
+                end
+            end
         end
     end
     puts result
 end
 
-substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
