472aba806f9d2ce67f97260d65cef19caf8f7c27
KarolisGaiv <kgaivenis@gmail.com>
2021-04-01 20:27:21 +0300

    Clarify variable names and their values

--- bubble_sort/sort.rb
+++ bubble_sort/sort.rb
@@ -5,11 +5,9 @@ arr = [4,3,78,2,0,2]
 
 def bubble_sort(arr)
     arr.each do |i|
-        next_el = arr.index(i) + 1
-        puts "Current element is #{arr.index(i)} "
-        puts "Next element is #{next_el}"
-        puts "This is curent element's value #{i}. And this is next elements value #{arr[next_el]}"
-        
+        next_index = arr.index(i) + 1
+        next_value = arr[arr.index(i) + 1] 
+        puts "Sekantis indexas yra #{next_index}, o jo verte #{next_value}"
     end
 end
 
